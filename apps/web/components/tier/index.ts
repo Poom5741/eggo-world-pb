3d795062e94e92bc7237e53e6392a5603f978e4d
Poom5741 <poom@charoenyost.com>
2026-04-22 17:31:12 +0700

    feat(22-03): create tier components barrel export
    - Clean import path: @/components/tier
    - Exports TierBadgeCard, TierProgressBar, TierClaimButton
    - Exports notification and summary components

---
+++ apps/web/components/tier/index.ts
@@ -0,0 +1,4 @@
+// Tier components barrel export
+export { TierBadgeCard, TierBadgeGrid, type TierBadge } from './TierBadgeCard'
+export { TierProgressBar, TierProgressSummary } from './TierProgressBar'
+export { TierClaimButton, TierClaimNotification } from './TierClaimButton'
